c54c3a8625ec7ec28fdcb6ca4fba1f5b02fe3ffb
nrot <nrot@DESKTOP-3MS9R6V>
2017-02-11 18:39:00 +0300

    Add project files.

---
+++ Game_rpg_Nrot/Game_rpg_Nrot.cpp
@@ -0,0 +1,16 @@
+// Game_rpg_Nrot.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <SDL.h>
+
+int main(int argc, char ** argv)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	// game code eventually goes here
+
+	SDL_Quit();
+
+	return 0;
+}
